7249f4f60aff970a2de0ea2c1262033ee60a5b6c
aliaksei-ivanouski <aliaksei.ivanouski@gmail.com>
2022-05-29 08:43:04 +0400

    main - add notification model;

---
+++ instaclone/Model/Notification.swift
@@ -0,0 +1,36 @@
+//
+//  Notification.swift
+//  instaclone
+//
+//  Created by Aliaksei Ivanouski on 29/05/2022.
+//
+
+import FirebaseFirestoreSwift
+import Firebase
+
+struct Notification: Identifiable, Decodable {
+    @DocumentID var id: String?
+    var postId: String?
+    let username: String
+    let profileImageUrl: String
+    let timestamp: Timestamp
+    let type: Int
+    let uid: String
+}
+
+enum NotificationType: Int, Decodable {
+    case like
+    case comment
+    case follow
+    
+    var notificationMessage: String {
+        switch self {
+        case .like:
+            return " liked one of your posts."
+        case .comment:
+            return " commented on one of your posts."
+        case .follow:
+            return " started following you."
+        }
+    }
+}
